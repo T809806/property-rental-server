d3aa3f49324adae3ee678c4a07d01392a1b4a967
T806809 <tahiyaakter94@gmail.com>
2026-06-26 23:31:11 +0600

    some issue fixed inside verifyRole.js

--- middleware/verifyRole.js
+++ middleware/verifyRole.js
@@ -1,12 +1,15 @@
 const verifyRole = (...roles) => {
+
   return (req, res, next) => {
 
-    console.log("USER ROLE =", req.user.role);
-    console.log("ALLOWED ROLES =", roles);
+      console.log("USER ROLE =", req.user.role);
+      console.log("ALLOWED ROLES =", roles);
 
-    if (!roles.includes(req.user.role)) {
+ if (!roles.includes(req.user.role)) {
+
       return res.status(403).json({
         message: "Access Denied",
+        
       });
     }
 
